9e0527465341b676ea3ae41a631faaefd2815f0e
KulikSS0395 <ds_347@mail.ru>
2022-10-20 15:20:03 +0300

    что то типа такого должно быть

--- src/main/java/com/example/crud_kulik/controller/AdminController.java
+++ src/main/java/com/example/crud_kulik/controller/AdminController.java
@@ -1,34 +1,15 @@
 package com.example.crud_kulik.controller;
 
-import com.example.crud_kulik.entity.Role;
-import com.example.crud_kulik.entity.User;
-import com.example.crud_kulik.service.RoleService;
-import com.example.crud_kulik.service.UserService;
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
 @RequestMapping("admin")
 @Log
 public class AdminController {
-
-    private final UserService userService;
-    private final RoleService roleService;
-
-    @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
-    }
 
     @GetMapping("")
     public String allUsers() {
--- src/main/java/com/example/crud_kulik/controller/UserController.java
+++ src/main/java/com/example/crud_kulik/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.example.crud_kulik.controller;
 
-import com.example.crud_kulik.entity.User;
 import com.example.crud_kulik.service.UserService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,13 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 @Log
 public class UserController {
-
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping()
     public String getUserPage() {
